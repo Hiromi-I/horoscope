99a221a942edb8c011c93de3f6b9077affa7fd2b
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2020-01-31 15:43:19 +0900

    body に全体のCSSを追加

--- src/App.vue
+++ src/App.vue
@@ -18,4 +18,15 @@ export default class App extends Vue {}
 
 <style lang="scss">
 @import "assets/styles/reset.css";
+@import "assets/styles/common";
+
+body {
+  background: url(assets/images/back.png) repeat;
+  min-height: 100vh;
+  padding: 100px 0 70px;
+  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-size: 14px;
+  line-height: 1.5;
+  color: #333;
+}
 </style>
