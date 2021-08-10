7edaed815f38429bc7a8afadc877b1e3eb053f44
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2021-08-11 00:36:24 +0900

    stateの書き換え方法を変更

--- tests/unit/App.spec.ts
+++ tests/unit/App.spec.ts
@@ -1,40 +1,44 @@
-import { shallowMount } from "@vue/test-utils";
+import { shallowMount, flushPromises } from "@vue/test-utils";
 import App from "@/App.vue";
 import DefaultContents from "@/components/DefaultContents.vue";
 import ResultContents from "@/components/ResultContents.vue";
 import ModalDialog from "@/components/ModalDialog.vue";
 
 describe("App", () => {
-  it("占い結果が無い場合はDefaultContentsを描画", () => {
+  it("占い結果が無い場合はDefaultContentsを描画", async () => {
     const wrapper = shallowMount(App);
 
-    wrapper.setData({ fortuneResult: null });
+    wrapper.vm.fortuneResult = null;
+    await flushPromises();
 
     expect(wrapper.findComponent(DefaultContents).exists()).toBeTruthy();
     expect(wrapper.findComponent(ResultContents).exists()).toBeFalsy();
   });
 
-  it("占い結果が有る場合はResultContentsを描画", () => {
+  it("占い結果が有る場合はResultContentsを描画", async () => {
     const wrapper = shallowMount(App);
 
-    wrapper.setData({ fortuneResult: {} });
+    wrapper.vm.fortuneResult = {};
+    await flushPromises();
 
     expect(wrapper.findComponent(ResultContents).exists()).toBeTruthy();
     expect(wrapper.findComponent(DefaultContents).exists()).toBeFalsy();
   });
 
-  it("メッセージが有る場合はModalDialogを表示", () => {
+  it("メッセージが有る場合はModalDialogを表示", async () => {
     const wrapper = shallowMount(App);
 
-    wrapper.setData({ errorMessage: "test" });
+    wrapper.vm.errorMessage = "test";
+    await flushPromises();
 
     expect(wrapper.findComponent(ModalDialog).isVisible()).toBeTruthy();
   });
 
-  it("メッセージが無い場合はModalDialogを非表示", () => {
+  it("メッセージが無い場合はModalDialogを非表示", async () => {
     const wrapper = shallowMount(App);
 
-    wrapper.setData({ errorMessage: "" });
+    wrapper.vm.errorMessage = "";
+    await flushPromises();
 
     expect(wrapper.findComponent(ModalDialog).isVisible()).toBeFalsy();
   });
