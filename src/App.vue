baeeb38d7b973771d6c4f74c06855f342fb75c9f
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2021-08-10 21:57:27 +0900

    日付変更からAPIコールまでをカスタムフックに切り出し

--- src/App.vue
+++ src/App.vue
@@ -17,23 +17,22 @@
     <AppFooter />
 
     <ModalDialog
-      v-show="dialogMessage"
-      :message="dialogMessage"
+      v-show="errorMessage"
+      :message="errorMessage"
       @close="clearMessage"
     />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue";
-import axios from "axios";
+import { defineComponent } from "vue";
 
 import AppHeader from "@/components/AppHeader.vue";
 import AppFooter from "@/components/AppFooter.vue";
 import DefaultContents from "@/components/DefaultContents.vue";
 import ResultContents from "@/components/ResultContents.vue";
 import ModalDialog from "@/components/ModalDialog.vue";
-import { Fortune, DailyResult, SignResult } from "@/horoscope";
+import { useGetFortune } from "@/hooks/useGetFortune";
 
 export default defineComponent({
   name: "App",
@@ -45,47 +44,13 @@ export default defineComponent({
     ModalDialog,
   },
   setup() {
-    const fortuneResult = ref<Fortune | null>(null);
-    const dialogMessage = ref("");
-    const onDateUpdate = (dateString: string) => {
-      fortuneResult.value = null;
-
-      if (dateString) {
-        const { year, month, day } = parseDate(dateString);
-        getFortune(year, month, day);
-      }
-    };
-
-    type parseDateFunc = (dateString: string) => { year: string; month: string; day: string };
-    const parseDate: parseDateFunc = (dateString: string) => {
-      const array = dateString.split("-");
-      return { year: array[0], month: array[1], day: array[2] };
-    }
-
-    type getFortuneFunc = (year: string, month: string, day: string) => Promise<void>;
-    const getFortune: getFortuneFunc = async (year: string, month: string, day: string) => {
-      const url = `/api/v1/fortune/?year=${year}&month=${month}&day=${day}`;
-      try {
-        const response = await axios.get<Fortune>(url);
-        fortuneResult.value = response.data;
-      } catch (error) {
-        if (error.response.data === "Date Is Too Far") {
-          dialogMessage.value =
-            "指定された日時の占いデータは見つかりませんでした。\n近日の日時を指定して下さい。";
-        } else {
-          dialogMessage.value =
-            "占いデータの取得に失敗しました。\n時間を置いて、再度ご確認ください。";
-        }
-      }
-    }
-
-    const clearMessage = () => {
-      dialogMessage.value = "";
-    };
+    const { fortuneResult, errorMessage, isLoading, onDateUpdate } = useGetFortune();
+    const clearMessage = () => errorMessage.value = "";
 
     return {
       fortuneResult,
-      dialogMessage,
+      errorMessage,
+      isLoading,
       onDateUpdate,
       clearMessage,
     };
