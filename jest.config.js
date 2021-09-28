25181691067b4d48095362e1d54fbffdd7141935
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2021-09-28 22:48:28 +0900

    テスト結果を取得

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,18 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ["src/**/*.{js,ts}"],
   coverageDirectory: "coverage",
+  reporters: [
+    "default",
+    [
+      "jest-junit",
+      {
+        suiteName: "jest tests",
+        outputDirectory: "reports/jest",
+        outputName: "js-test-results.xml",
+        classNameTemplate: "{classname}-{title}",
+        titleTemplate: "{classname}-{title}",
+        ancestorSeparator: " › ",
+      },
+    ],
+  ],
 };
